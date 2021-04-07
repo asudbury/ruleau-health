6f8f066d945df85e59c13568a148545399b807af
Adrian Sudbury <sudburya@gmail.com>
2021-04-07 16:20:56 +0100

    Override history update

--- src/components/case/CaseRuleOverrideHistory.tsx
+++ src/components/case/CaseRuleOverrideHistory.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  makeStyles,
   Grid,
   TableContainer,
   Table,
@@ -9,8 +10,26 @@ import {
   Typography,
 } from "@material-ui/core";
 import SubjectIcon from "@material-ui/icons/Subject";
+import CheckCircleOutlineOutlinedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
+import HighlightOffOutlinedIcon from "@material-ui/icons/HighlightOffOutlined";
+
+const useStyles = makeStyles((theme) => ({
+  error: {
+    color: theme.palette.error.main,
+  },
+  success: {
+    color: theme.palette.success.main,
+  },
+  nowrap: {
+    display: "flex",
+    whiteSpace: "nowrap",
+    overflow: "auto",
+  },
+}));
 
 export default function CaseRuleOverrideHistory() {
+  const classes = useStyles();
+
   return (
     <Grid container spacing={1}>
       <Grid item xs={11}>
@@ -28,9 +47,17 @@ export default function CaseRuleOverrideHistory() {
                 >
                   <SubjectIcon color="primary" /> 01 Jan 2020
                 </TableCell>
+                <TableCell>
+                  <div className={classes.nowrap}>
+                    <CheckCircleOutlineOutlinedIcon
+                      fontSize="small"
+                      className={classes.success}
+                    />
+                    Applied
+                  </div>
+                </TableCell>
                 <TableCell>Morgan Atkins</TableCell>
                 <TableCell>lorem ipsum</TableCell>
-                <TableCell>Applied</TableCell>
               </TableRow>
               <TableRow>
                 <TableCell
@@ -42,9 +69,18 @@ export default function CaseRuleOverrideHistory() {
                 >
                   <SubjectIcon color="primary" /> 01 Apr 2020
                 </TableCell>
+                <TableCell>
+                  {" "}
+                  <div className={classes.nowrap}>
+                    <HighlightOffOutlinedIcon
+                      fontSize="small"
+                      className={classes.error}
+                    />
+                    Removed
+                  </div>
+                </TableCell>
                 <TableCell>Lucy Cox</TableCell>
                 <TableCell>lorem ipsum</TableCell>
-                <TableCell>Removed</TableCell>
               </TableRow>
             </TableBody>
           </Table>
