cce51eec575da5a5d8a9a776f01d38abb45d0e9e
Adrian Sudbury <sudburya@gmail.com>
2021-05-24 14:42:48 +0100

    Amendment to Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,79 +1,55 @@
 import { Router, Route, Switch } from "react-router-dom";
 import history from "../src/utils/History";
-import HomePage from "../src/pages/HomePage";
+import HomePage from "./pages/HomePage";
 import ProcessPage from "./pages/ProcessPage";
 import CasePage from "./pages/CasePage";
 import LogViewer from "../src/components/LogViewer";
+import PageNotFound from "../src/components/PageNotFound";
+import ErrorPage from "../src/components/ErrorPage";
 
 export default function Routes(): JSX.Element {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/ruleau-health" component={HomePage} />
+        <Route exact path={["/", "/ruleau-health"]} component={HomePage} />
+
         <Route
           exact
-          path="/ruleau-health/process/:processId"
-          component={ProcessPage}
-        />
-        <Route exact path="/process/:processId" component={ProcessPage} />
-        <Route
-          exact
-          path="/ruleau-health/process/:processId"
-          component={ProcessPage}
-        />
-        <Route exact path="/process/:processId/cases" component={ProcessPage} />
-        <Route
-          exact
-          path="/ruleau-health/process/:processId/cases"
-          component={ProcessPage}
-        />
-        <Route exact path="/process/:processId/rules" component={ProcessPage} />
-        <Route
-          exact
-          path="/ruleau-health/process/:processId/rules"
+          path={[
+            "/process/:processId",
+            "/ruleau-health/process/:processId",
+            "/process/:processId/cases",
+            "/ruleau-health/process/:processId/cases",
+            "/process/:processId/rules",
+            "/ruleau-health/process/:processId/rules",
+            "/process/:processId/rules",
+            "/process/:processId/statistics",
+            "/ruleau-health/process/:processId/statistics",
+            "/process/:processId/overview",
+            "/ruleau-health/process/:processId/overview",
+          ]}
           component={ProcessPage}
         />
         <Route
           exact
-          path="/process/:processId/statistics"
-          component={ProcessPage}
-        />
-        <Route
-          exact
-          path="/ruleau-health/process/:processId/statistics"
-          component={ProcessPage}
-        />
-        <Route
-          exact
-          path="/process/:processId/overview"
-          component={ProcessPage}
-        />
-        <Route
-          exact
-          path="/ruleau-health/process/:processId/overview"
-          component={ProcessPage}
-        />
-        <Route
-          exact
-          path="/process/:processId/case/:caseId"
+          path={[
+            "/process/:processId/case/:caseId",
+            "/ruleau-health/process/:processId/case/:caseId",
+          ]}
           component={CasePage}
         />
         <Route
           exact
-          path="/ruleau-health/process/:processId/case/:caseId"
-          component={CasePage}
-        />
-                <Route
-          exact
-          path="/log"
+          path={["/log", "/ruleau-health/log"]}
           component={LogViewer}
         />
+
         <Route
           exact
-          path="/ruleau-health/log"
-          component={LogViewer}
+          path={["/error", "/ruleau-health/error"]}
+          component={ErrorPage}
         />
+        <Route component={PageNotFound} />
       </Switch>
     </Router>
   );
